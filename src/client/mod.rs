9cd8967dd380e226f333869f43c00e28d821fb2b
Gianluca Klimmer <pixelmannvip@gmail.com>
2024-09-20 00:10:11 +0200

    add wrapper plugins

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,6 +1,30 @@
+use bevy::prelude::*;
+
+use animation::AnimationPlugin;
+use camera::CameraPlugin;
+use input::InputPlugin;
+use king::KingPlugin;
+use networking::ClientNetworkingPlugin;
+
+use crate::shared::networking::setup_level;
+
 pub mod animation;
 pub mod camera;
 pub mod gizmos;
 pub mod input;
 pub mod king;
 pub mod networking;
+
+pub struct ClientPlugin;
+
+impl Plugin for ClientPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(KingPlugin);
+        app.add_plugins(ClientNetworkingPlugin);
+        app.add_plugins(CameraPlugin);
+        app.add_plugins(InputPlugin);
+        app.add_plugins(AnimationPlugin);
+
+        app.add_systems(Startup, setup_level);
+    }
+}
