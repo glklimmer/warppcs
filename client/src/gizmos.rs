5629a349d91523a85b8aa6be3441d97c80a74658
Gianluca Klimmer <31933341+glklimmer@users.noreply.github.com>
2024-12-28 22:54:25 +0100

    add gizmos settings

--- client/src/gizmos.rs
+++ client/src/gizmos.rs
@@ -1,14 +1,16 @@
 use bevy::{
-    color::palettes::css::{BLUE, RED},
+    color::palettes::css::{BLUE, GREEN, RED},
     prelude::*,
 };
 
-use shared::BoxCollider;
+use shared::{server::ai::SIGHT_RANGE, BoxCollider};
 
 pub struct GizmosPlugin;
 
 impl Plugin for GizmosPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GizmosSettings::default());
+
         app.add_systems(Update, (draw_range, draw_collider));
     }
 }
@@ -16,14 +18,33 @@ impl Plugin for GizmosPlugin {
 #[derive(Component)]
 pub struct UnitRange(pub f32);
 
-fn draw_range(mut gizmos: Gizmos, query: Query<(&Transform, &UnitRange)>) {
+#[derive(Resource, Default)]
+pub struct GizmosSettings {
+    pub on: bool,
+}
+
+fn draw_range(
+    mut gizmos: Gizmos,
+    settings: Res<GizmosSettings>,
+    query: Query<(&Transform, &UnitRange)>,
+) {
+    if !settings.on {
+        return;
+    }
     for (transform, range) in query.iter() {
         gizmos.circle_2d(transform.translation.truncate(), range.0, RED);
-        // gizmos.circle_2d(transform.translation.truncate(), SIGHT_RANGE, GREEN);
+        gizmos.circle_2d(transform.translation.truncate(), SIGHT_RANGE, GREEN);
     }
 }
 
-fn draw_collider(mut gizmos: Gizmos, query: Query<(&Transform, &BoxCollider)>) {
+fn draw_collider(
+    mut gizmos: Gizmos,
+    settings: Res<GizmosSettings>,
+    query: Query<(&Transform, &BoxCollider)>,
+) {
+    if !settings.on {
+        return;
+    }
     for (transform, collider) in query.iter() {
         gizmos.rect_2d(transform.translation.truncate(), 0., collider.0, BLUE);
     }
--- client/src/input.rs
+++ client/src/input.rs
@@ -2,13 +2,15 @@ use bevy::prelude::*;
 
 use shared::networking::{PlayerCommand, PlayerInput};
 
+use crate::gizmos::GizmosSettings;
+
 pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(PlayerInput::default());
         app.add_event::<PlayerCommand>();
-        app.add_systems(Update, player_input);
+        app.add_systems(Update, (player_input, gizmos_settings));
     }
 }
 
@@ -32,5 +34,14 @@ fn player_input(
 
     if keyboard_input.just_pressed(KeyCode::KeyF) {
         player_commands.send(PlayerCommand::Interact);
+    }
+}
+
+fn gizmos_settings(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut settings: ResMut<GizmosSettings>,
+) {
+    if keyboard_input.just_pressed(KeyCode::KeyY) {
+        settings.on = !settings.on;
     }
 }
