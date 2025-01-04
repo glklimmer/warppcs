9b2c435decb730f8d9013e818d81cabe54015ead
Gianluca Frongia <20201125+GianlucaFrongia@users.noreply.github.com>
2025-01-04 16:04:53 +0100

    refactor flag

--- client/src/networking/mod.rs
+++ client/src/networking/mod.rs
@@ -14,7 +14,7 @@ use shared::{
 use std::collections::HashMap;
 
 use crate::{
-    animations::{Change, EntityChangeEvent},
+    animations::{king::KingAnimation, Change, EntityChangeEvent},
     entities::PartOfScene,
 };
 
