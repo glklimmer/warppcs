31dde6e99951ab88b55e83e3ff6177021565a88e
Gianluca Klimmer <pixelmannvip@gmail.com>
2025-03-25 22:16:45 +0100

    change init sprite triggers to make more sense

--- client/src/entities/spawn.rs
+++ client/src/entities/spawn.rs
@@ -94,8 +94,8 @@ fn init_local_player(
 }
 
 fn init_player_sprite(
-    trigger: Trigger<OnAdd, Sprite>,
-    mut players: Query<&mut Sprite, With<PhysicalPlayer>>,
+    trigger: Trigger<OnAdd, PhysicalPlayer>,
+    mut players: Query<&mut Sprite>,
     mut commands: Commands,
     king_sprite_sheet: Res<KingSpriteSheet>,
 ) {
@@ -114,7 +114,7 @@ fn init_player_sprite(
 }
 
 fn init_building_sprite(
-    trigger: Trigger<OnAdd, Sprite>,
+    trigger: Trigger<OnAdd, Building>,
     mut buildings: Query<(&mut Sprite, &Building, &BuildStatus)>,
     asset_server: Res<AssetServer>,
 ) {
@@ -135,7 +135,7 @@ fn update_building_sprite(
 }
 
 fn init_unit_sprite(
-    trigger: Trigger<OnAdd, Sprite>,
+    trigger: Trigger<OnAdd, Unit>,
     mut units: Query<(&mut Sprite, &Unit)>,
     sprite_sheets: Res<UnitSpriteSheets>,
     mut commands: Commands,
@@ -156,9 +156,9 @@ fn init_unit_sprite(
 }
 
 fn init_flag_sprite(
-    trigger: Trigger<OnAdd, Sprite>,
+    trigger: Trigger<OnAdd, Flag>,
     mut commands: Commands,
-    mut flag: Query<&mut Sprite, With<Flag>>,
+    mut flag: Query<&mut Sprite>,
     flag_sprite_sheet: Res<FlagSpriteSheet>,
 ) {
     let Ok((mut sprite)) = flag.get_mut(trigger.entity()) else {
@@ -181,225 +181,6 @@ fn init_flag_sprite(
         Highlightable::default(),
     ));
 }
-
-// #[allow(clippy::too_many_arguments)]
-// fn spawn(
-//     mut network_events: EventReader<NetworkEvent>,
-//     mut spawn_player: EventWriter<SpawnPlayer>,
-//     mut spawn_unit: EventWriter<SpawnUnit>,
-//     mut spawn_mount: EventWriter<SpawnMount>,
-//     mut spawn_projectile: EventWriter<SpawnProjectile>,
-//     mut spawn_flag: EventWriter<SpawnFlag>,
-//     mut pick_flag: EventWriter<PickFlag>,
-//     mut drop_flag: EventWriter<DropFlag>,
-// ) {
-//     for event in network_events.read() {
-//         match &event.message {
-//             ServerMessages::SpawnPlayer(spawn) => {
-//                 spawn_player.send(spawn.clone());
-//             }
-//             ServerMessages::SpawnUnit(spawn) => {
-//                 spawn_unit.send(spawn.clone());
-//             }
-//             ServerMessages::SpawnMount(spawn) => {
-//                 spawn_mount.send(spawn.clone());
-//             }
-//             ServerMessages::SpawnProjectile(spawn) => {
-//                 spawn_projectile.send(spawn.clone());
-//             }
-//             ServerMessages::SpawnFlag(spawn) => {
-//                 spawn_flag.send(spawn.clone());
-//             }
-//             ServerMessages::PickFlag(pick) => {
-//                 pick_flag.send(pick.clone());
-//             }
-//             ServerMessages::DropFlag(drop) => {
-//                 drop_flag.send(drop.clone());
-//             }
-//             ServerMessages::SpawnGroup { player, units } => {
-//                 spawn_player.send(player.clone());
-//
-//                 for unit in units {
-//                     spawn_unit.send(unit.clone());
-//                 }
-//             }
-//             _ => (),
-//         }
-//     }
-// }
-//
-// fn spawn_player(
-//     mut commands: Commands,
-//     mut spawn_player: EventReader<SpawnPlayer>,
-//     mut lobby: ResMut<ClientPlayers>,
-//     mut network_mapping: ResMut<NetworkMapping>,
-//     client_id: Res<CurrentClientId>,
-//     king_sprite_sheet: Res<KingSpriteSheet>,
-//     camera: Query<Entity, With<Camera>>,
-// ) {
-//     let client_id = client_id.0;
-//     for spawn in spawn_player.read() {
-//         let SpawnPlayer {
-//             id,
-//             translation,
-//             entity: server_player_entity,
-//             mounted,
-//         } = spawn;
-//
-//         let mut client_player_entity = commands.spawn((
-//             SpriteAnimationBundle::new(
-//                 translation,
-//                 &king_sprite_sheet.sprite_sheet,
-//                 match mounted {
-//                     Some(mounted) => match mounted.mount_type {
-//                         MountType::Horse => KingAnimation::HorseIdle,
-//                     },
-//                     None => KingAnimation::Idle,
-//                 },
-//                 3.,
-//             ),
-//             PartOfScene,
-//         ));
-//
-//         if let Some(mounted) = mounted {
-//             client_player_entity.insert(Mounted {
-//                 mount_type: mounted.mount_type,
-//             });
-//         }
-//
-//         let player_entity = client_player_entity.id();
-//
-//         if client_id.eq(id) {
-//             client_player_entity.insert(ControlledPlayer);
-//             commands
-//                 .entity(camera.single())
-//                 .insert(CameraFollow::fixed(player_entity));
-//         }
-//
-//         let player_info = PlayerEntityMapping {
-//             server_entity: *server_player_entity,
-//             client_entity: player_entity,
-//         };
-//
-//         lobby.players.insert(*id, player_info);
-//         network_mapping
-//             .0
-//             .insert(*server_player_entity, player_entity);
-//     }
-// }
-//
-
-//
-// fn spawn_mount(
-//     mut commands: Commands,
-//     mut spawn_unit: EventReader<SpawnMount>,
-//     mut network_mapping: ResMut<NetworkMapping>,
-//     sprite_sheet: Res<HorseSpriteSheet>,
-// ) {
-//     for spawn in spawn_unit.read() {
-//         let SpawnMount {
-//             entity: server_unit_entity,
-//             mount_type,
-//             translation,
-//         } = spawn;
-//
-//         let client_unit_entity = commands
-//             .spawn((
-//                 Unit,
-//                 SpriteAnimationBundle::new(
-//                     translation,
-//                     &sprite_sheet.sprite_sheet,
-//                     HorseAnimation::Idle,
-//                     3.,
-//                 ),
-//                 *mount_type,
-//             ))
-//             .id();
-//
-//         network_mapping
-//             .0
-//             .insert(*server_unit_entity, client_unit_entity);
-//     }
-// }
-//
-// fn spawn_projectile(
-//     mut commands: Commands,
-//     asset_server: Res<AssetServer>,
-//     mut network_mapping: ResMut<NetworkMapping>,
-//     mut spawn_projectile: EventReader<SpawnProjectile>,
-// ) {
-//     for spawn in spawn_projectile.read() {
-//         let SpawnProjectile {
-//             entity: server_entity,
-//             projectile_type,
-//             translation,
-//             direction,
-//         } = spawn;
-//         let texture = match projectile_type {
-//             ProjectileType::Arrow => asset_server.load("sprites/arrow.png"),
-//         };
-//
-//         let direction: Vec2 = (*direction).into();
-//         let position: Vec3 = (*translation).into();
-//         let position = position.truncate();
-//
-//         let angle = (direction - position).angle_to(position);
-//
-//         let client_entity = commands
-//             .spawn((
-//                 Projectile,
-//                 Sprite {
-//                     image: texture,
-//                     ..default()
-//                 },
-//                 Transform {
-//                     translation: (*translation).into(),
-//                     scale: Vec3::splat(2.0),
-//                     rotation: Quat::from_rotation_z(angle),
-//                 },
-//             ))
-//             .id();
-//
-//         network_mapping.0.insert(*server_entity, client_entity);
-//     }
-// }
-//
-// fn spawn_flag(
-//     mut commands: Commands,
-//     mut network_mapping: ResMut<NetworkMapping>,
-//     mut spawn_flag: EventReader<SpawnFlag>,
-//     flag_sprite_sheet: Res<FlagSpriteSheet>,
-//     client_id: Res<CurrentClientId>,
-//     lobby: Res<ClientPlayers>,
-// ) {
-//     let client_id = client_id.0;
-//     for spawn in spawn_flag.read() {
-//         let SpawnFlag {
-//             flag: server_flag_entity,
-//         } = spawn;
-//
-//         let client_flag_entity = commands
-//             .spawn((
-//                 Flag,
-//                 SpriteAnimationBundle::new(
-//                     &[0., 0., Layers::Flag.as_f32()],
-//                     &flag_sprite_sheet.sprite_sheet,
-//                     FlagAnimation::Wave,
-//                     0.2,
-//                 ),
-//             ))
-//             .id();
-//
-//         let player_entity = lobby.players.get(&client_id).unwrap().client_entity;
-//         if let Some(mut player) = commands.get_entity(player_entity) {
-//             player.add_child(client_flag_entity);
-//         }
-//
-//         network_mapping
-//             .0
-//             .insert(*server_flag_entity, client_flag_entity);
-//     }
-// }
 
 fn pick_flag(
     mut commands: Commands,
