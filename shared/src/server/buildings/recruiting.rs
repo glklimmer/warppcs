8fb45e70d2b2424677462b2bfb21585a3e86283a
Chicho <chicho@Mac.fritz.box>
2025-05-06 19:25:51 +0200

    add missing import

--- shared/src/server/buildings/recruiting.rs
+++ shared/src/server/buildings/recruiting.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::entity::MapEntities, prelude::*};
 
 use bevy::sprite::Anchor;
 use bevy_replicon::prelude::{Replicated, SendMode, ServerTriggerExt, ToClients};
