ec69a471e5a4441c4a2b6436fdd3c63e2d9d0a1e
Chicho <gianluca.frongia@protonmail.com>
2025-05-11 20:27:58 +0200

    Add ppc_console

---
+++ ppc_console/src/main.rs
@@ -0,0 +1,103 @@
+use bevy_remote::{BrpRequest, http::DEFAULT_ADDR, http::DEFAULT_PORT};
+use clap::{CommandFactory, Parser, Subcommand, ValueHint, arg, command};
+use clap_complete::{Shell, generate};
+use console_protocol::*;
+use dialoguer::Select;
+use serde_json::{Value, to_value};
+use std::{fs::File, path::PathBuf};
+
+#[derive(Parser)]
+#[command(name = "ppc", about = "The warppc first tooling", version = "0.1.0")]
+pub struct PPC {
+    #[arg(long, default_value = "DEFAULT_ADDR.to_string()")]
+    host: String,
+
+    #[arg(long, default_value_t = DEFAULT_PORT)]
+    port: u16,
+
+    #[command(subcommand)]
+    pub command: PPCSubCommands,
+}
+
+#[derive(Subcommand)]
+pub enum PPCSubCommands {
+    RandomItem {},
+    Print {
+        #[arg(short, long, value_hint = ValueHint::CommandWithArguments)]
+        message: String,
+    },
+    SpawnUnit {
+        #[arg(short, long, value_hint = ValueHint::CommandWithArguments)]
+        unit: bool,
+    },
+}
+
+fn main() {
+    let cli = PPC::parse();
+
+    let host_part = format!("{}:{}", DEFAULT_ADDR.to_string(), DEFAULT_PORT);
+    let url = format!("http://{}/", host_part);
+    let home_dir = dirs::home_dir().expect("Could not find home directory");
+
+    let mut zsh_completion_dir = PathBuf::from(home_dir);
+    zsh_completion_dir.push(".zsh");
+    zsh_completion_dir.push("completion");
+    zsh_completion_dir.push("ppc");
+    let mut output_file =
+        File::create(&zsh_completion_dir).expect(&format!("Could not create file: "));
+
+    generate(Shell::Zsh, &mut PPC::command(), "ppc", &mut output_file);
+
+    match cli.command {
+        PPCSubCommands::RandomItem {} => {
+            let req = BrpRequest {
+                jsonrpc: String::from("2.0"),
+                method: BRP_TRIGGER_RANDOM_ITEMS.into(),
+                id: None,
+                params: None,
+            };
+            let res = ureq::post(&url)
+                .send_json(req)
+                .unwrap()
+                .body_mut()
+                .read_json::<Value>()
+                .unwrap();
+
+            println!("{:#}", res);
+        }
+        PPCSubCommands::Print { message } => println!("{}", message),
+        PPCSubCommands::SpawnUnit { unit } => {
+            let unit_spawn = match unit {
+                true => {
+                    let items = vec!["archer", "pikemen", "shield"];
+                    let selection = Select::new()
+                        .with_prompt("Which unit?")
+                        .items(&items)
+                        .interact()
+                        .unwrap();
+
+                    items[selection].to_string()
+                }
+                false => String::from("archer"),
+            };
+
+            let req = BrpRequest {
+                jsonrpc: String::from("2.0"),
+                method: BRP_TRIGGER_SPAWN_UNIT.into(),
+                id: None,
+                params: Some(
+                    to_value(BrpSpawnUnit { unit: unit_spawn })
+                        .expect("Unable to convert query parameters to a valid JSON value"),
+                ),
+            };
+            let res = ureq::post(&url)
+                .send_json(req)
+                .unwrap()
+                .body_mut()
+                .read_json::<Value>()
+                .unwrap();
+
+            println!("{:#}", res);
+        }
+    };
+}
