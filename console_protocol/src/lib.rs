04db08edad3eb1e1137ad2f2a60a3ef2caea753e
Chicho <gianluca.frongia@protonmail.com>
2025-05-11 20:26:59 +0200

    Add console protocol

---
+++ console_protocol/src/lib.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+pub const BRP_TRIGGER_SPAWN_UNIT: &str = "trigger/spawn_unit";
+
+pub const BRP_TRIGGER_RANDOM_ITEMS: &str = "trigger/random_items";
+
+#[derive(Serialize, Deserialize)]
+pub struct BrpSpawnUnit {
+    pub unit: String,
+}
