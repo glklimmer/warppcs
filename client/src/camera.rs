7a09dea2e9181137c55e62580b2782935626d3c1
Chicho <gianluca.frongia@protonmail.com>
2025-07-04 22:42:54 +0200

    BackgroundPlugin

--- client/src/camera.rs
+++ client/src/camera.rs
@@ -1,8 +1,5 @@
 use bevy::prelude::*;
-use bevy_parallax::{
-    CreateParallaxEvent, LayerData, LayerRepeat, LayerSpeed, ParallaxCameraComponent,
-    RepeatStrategy,
-};
+use bevy_parallax::ParallaxCameraComponent;
 
 pub struct CameraPlugin;
 
@@ -12,8 +9,8 @@ impl Plugin for CameraPlugin {
     }
 }
 
-fn setup_camera(mut commands: Commands, mut create_parallax: EventWriter<CreateParallaxEvent>) {
-    let camera = commands
+fn setup_camera(mut commands: Commands) {
+    commands
         .spawn((
             Camera2d,
             Projection::Orthographic(OrthographicProjection {
@@ -21,76 +18,5 @@ fn setup_camera(mut commands: Commands, mut create_parallax: EventWriter<CreateP
                 ..OrthographicProjection::default_2d()
             }),
         ))
-        .insert(ParallaxCameraComponent::default())
-        .id();
-    // TODO: Fix Parralax when travelling
-    let event = CreateParallaxEvent {
-        layers_data: vec![
-            LayerData {
-                speed: LayerSpeed::Horizontal(0.9),
-                repeat: LayerRepeat::horizontally(RepeatStrategy::MirrorBoth),
-                path: "background/sky.png".to_string(),
-                tile_size: UVec2::new(320, 240),
-                position: Vec2::new(0., 80.),
-                z: -5.0,
-                ..default()
-            },
-            LayerData {
-                speed: LayerSpeed::Horizontal(0.6),
-                repeat: LayerRepeat::horizontally(RepeatStrategy::MirrorBoth),
-                path: "background/far-clouds.png".to_string(),
-                tile_size: UVec2::new(128, 240),
-                position: Vec2::new(0., 80.),
-                z: -4.0,
-                ..default()
-            },
-            LayerData {
-                speed: LayerSpeed::Horizontal(0.55),
-                repeat: LayerRepeat::horizontally(RepeatStrategy::MirrorBoth),
-                path: "background/near-clouds.png".to_string(),
-                tile_size: UVec2::new(144, 240),
-                position: Vec2::new(0., 80.),
-                z: -3.0,
-                ..default()
-            },
-            LayerData {
-                speed: LayerSpeed::Horizontal(0.45),
-                repeat: LayerRepeat::horizontally(RepeatStrategy::MirrorBoth),
-                path: "background/far-mountains.png".to_string(),
-                tile_size: UVec2::new(160, 240),
-                position: Vec2::new(0., 80.),
-                z: -3.0,
-                ..default()
-            },
-            LayerData {
-                speed: LayerSpeed::Horizontal(0.40),
-                repeat: LayerRepeat::horizontally(RepeatStrategy::MirrorBoth),
-                path: "background/mountains.png".to_string(),
-                tile_size: UVec2::new(320, 240),
-                position: Vec2::new(0., 80.),
-                z: -2.0,
-                ..default()
-            },
-            LayerData {
-                speed: LayerSpeed::Horizontal(0.1),
-                repeat: LayerRepeat::horizontally(RepeatStrategy::MirrorBoth),
-                path: "background/trees.png".to_string(),
-                tile_size: UVec2::new(240, 240),
-                position: Vec2::new(0., 80.),
-                z: -1.0,
-                ..default()
-            },
-            LayerData {
-                speed: LayerSpeed::Horizontal(0.),
-                repeat: LayerRepeat::horizontally(RepeatStrategy::MirrorBoth),
-                path: "background/ground.png".to_string(),
-                tile_size: UVec2::new(640, 360),
-                position: Vec2::new(0., 129.),
-                z: -1.0,
-                ..default()
-            },
-        ],
-        camera,
-    };
-    create_parallax.write(event);
+        .insert(ParallaxCameraComponent::default());
 }
