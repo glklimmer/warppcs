1d00ade5c3f2e4badc146489a5d862cfcc5417cb
Gianluca Klimmer <pixelmannvip@gmail.com>
2025-09-19 16:28:07 +0200

    Add commander units respawn

--- shared/src/server/buildings/respawn.rs
+++ shared/src/server/buildings/respawn.rs
@@ -10,6 +10,7 @@ use crate::{
     },
     server::{
         ai::UnitBehaviour,
+        entities::commander::ArmyFlagAssignments,
         players::items::{CalculatedStats, Effect, Item},
     },
 };
@@ -32,6 +33,8 @@ pub fn respawn_units(
     )>,
     mut respawn_zones: Query<(&mut RespawnZone, &Transform, &BoxCollider, &Owner)>,
     original_building: Query<(&Building, &ItemAssignment)>,
+    commander: Query<&ArmyFlagAssignments>,
+    flag_query: Query<(&Flag, Option<&FlagUnits>)>,
 ) {
     for (flag_entity, flag, flag_transform, flag_collider, flag_owner, maybe_flag_units) in
         flags.iter()
@@ -58,34 +61,84 @@ pub fn respawn_units(
 
         recruit_component.respawn_timer_reset();
 
-        let Ok((building, assignment)) = original_building.get(flag.original_building) else {
-            // commander
-            continue;
+        match original_building.get(flag.original_building) {
+            Ok((building, assignment)) => {
+                respawn_for_flag(
+                    commands.reborrow(),
+                    flag_entity,
+                    flag,
+                    flag_owner,
+                    maybe_flag_units,
+                    respawn_transform,
+                    building,
+                    assignment,
+                );
+            }
+
+            Err(_) => {
+                // Commander
+                let Some(flag_units) = maybe_flag_units else {
+                    continue;
+                };
+                let Some(entity) = flag_units.first() else {
+                    continue;
+                };
+                let Ok(assignment) = commander.get(*entity) else {
+                    continue;
+                };
+                for flag_entity in assignment.flags.iter().flatten() {
+                    let (flag, maybe_flag_units) = flag_query.get(*flag_entity).unwrap();
+                    let (building, assignment) =
+                        original_building.get(flag.original_building).unwrap();
+
+                    respawn_for_flag(
+                        commands.reborrow(),
+                        *flag_entity,
+                        flag,
+                        flag_owner,
+                        maybe_flag_units,
+                        respawn_transform,
+                        building,
+                        assignment,
+                    );
+                }
+            }
         };
+    }
+}
 
-        let items: Vec<Item> = assignment.items.clone().into_iter().flatten().collect();
+fn respawn_for_flag(
+    mut commands: Commands,
+    flag_entity: Entity,
+    flag: &Flag,
+    flag_owner: &Owner,
+    maybe_flag_units: Option<&FlagUnits>,
+    respawn_transform: &Transform,
+    building: &Building,
+    assignment: &ItemAssignment,
+) {
+    let items: Vec<Item> = assignment.items.clone().into_iter().flatten().collect();
 
-        let num_alive = match maybe_flag_units {
-            Some(units) => (**units).len() as i32,
-            None => 0,
-        };
-        let max_allowed = items.calculated(Effect::UnitAmount) as i32;
+    let num_alive = match maybe_flag_units {
+        Some(units) => (**units).len() as i32,
+        None => 0,
+    };
+    let max_allowed = items.calculated(Effect::UnitAmount) as i32;
 
-        if num_alive < max_allowed {
-            let (unit, health, speed, damage, range) =
-                unit_stats(building.unit_type().unwrap(), &items, flag.color);
+    if num_alive < max_allowed {
+        let (unit, health, speed, damage, range) =
+            unit_stats(building.unit_type().unwrap(), &items, flag.color);
 
-            commands.spawn((
-                respawn_transform.translation.with_layer(Layers::Unit),
-                unit.clone(),
-                health,
-                speed,
-                damage,
-                range,
-                *flag_owner,
-                FlagAssignment(flag_entity),
-                UnitBehaviour::default(),
-            ));
-        }
+        commands.spawn((
+            respawn_transform.translation.with_layer(Layers::Unit),
+            unit.clone(),
+            health,
+            speed,
+            damage,
+            range,
+            *flag_owner,
+            FlagAssignment(flag_entity),
+            UnitBehaviour::default(),
+        ));
     }
 }
