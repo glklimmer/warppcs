fdf806c6915f529a1b5d6d6a9e89a4c0bf558feb
Gianluca Frongia <gianluca.frongia@protonmail.com>
2025-09-28 17:47:37 +0200

    Building sprite animations (#280)

--- client/src/animations/buildings/pikeman.rs
+++ client/src/animations/buildings/pikeman.rs
@@ -1,42 +1,40 @@
 use bevy::prelude::*;
 
-use shared::{enum_map::*, map::buildings::BuildStatus};
-
-use crate::animations::{
-    AnimationSpriteSheet, SpriteSheetAnimation, sprite_variant_loader::SpriteVariants,
+use shared::{
+    enum_map::*,
+    map::buildings::{BuildStatus, HealthIndicator},
 };
+
+use crate::{
+    anim,
+    animations::{AnimationSpriteSheet, sprite_variant_loader::SpriteVariants},
+};
+const ATLAS_COLUMNS: usize = 11;
 
 pub fn pikeman_building(world: &mut World) -> AnimationSpriteSheet<BuildStatus, SpriteVariants> {
     let asset_server = world.resource::<AssetServer>();
-    let texture = asset_server.load("sprites/buildings/pike_man_house.png");
+    let texture = asset_server.load("sprites/buildings/pikeman_house.png");
 
     let mut texture_atlas_layouts = world.resource_mut::<Assets<TextureAtlasLayout>>();
 
     let layout = texture_atlas_layouts.add(TextureAtlasLayout::from_grid(
-        UVec2::new(90, 50),
-        1,
-        1,
-        None,
+        UVec2::new(112, 50),
+        ATLAS_COLUMNS as u32,
+        7,
+        Some(UVec2::splat(1)),
         None,
     ));
 
     let animations = EnumMap::new(|c| match c {
-        BuildStatus::Marker => SpriteSheetAnimation {
-            first_sprite_index: 0,
-            ..default()
+        BuildStatus::Marker => anim!(0, 0),
+        BuildStatus::Constructing => anim!(2, 10),
+        BuildStatus::Built { indicator } => match indicator {
+            HealthIndicator::Healthy => anim!(1, 5),
+            HealthIndicator::Light => anim!(3, 5),
+            HealthIndicator::Medium => anim!(4, 5),
+            HealthIndicator::Heavy => anim!(5, 5),
         },
-        BuildStatus::Constructing => SpriteSheetAnimation {
-            first_sprite_index: 0,
-            ..default()
-        },
-        BuildStatus::Built { indicator: _ } => SpriteSheetAnimation {
-            first_sprite_index: 0,
-            ..default()
-        },
-        BuildStatus::Destroyed => SpriteSheetAnimation {
-            first_sprite_index: 0,
-            ..default()
-        },
+        BuildStatus::Destroyed => anim!(6, 0),
     });
 
     let animations_sound = EnumMap::new(|c| match c {
